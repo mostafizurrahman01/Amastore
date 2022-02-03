dbb279b0975073931bc3cc38f5dacbaf33a2fbe5
mostafizurrahman01 <abirmostafizurrahman@gmail.com>
2022-02-04 00:18:18 +0600

    Starting

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="title">
+            <h1 class="text-uppercase">Amastore</h1>
+        </div>
+    );
+};
+
+export default Header;
